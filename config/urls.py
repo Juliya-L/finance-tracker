e0c5928269b3b66e4dc7257136edd0e0ee58928f
Juliya-L <gilukju2@gmail.com>
2026-05-30 13:41:55 +0300

    Fix missing comma in urls

--- config/urls.py
+++ config/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('api/analytics/', include('analytics.urls')),
     path('api/budgets/', include('budgets.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema') name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
        ]
