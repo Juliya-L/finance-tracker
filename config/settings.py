cf227dc283a5ca253bdba65a357e6ab7a182d673
Juliya-L <gilukju2@gmail.com>
2026-06-05 01:26:55 +0300

    Test email in console

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 # Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
